c2862f9ebe25acf41144506fe55ffa8e01a5d459
Jakub Bak <245762@edu.p.lodz.pl>
2025-03-13 13:40:14 +0100

    Changed figure displaying to second screen (with additional import)

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/Job2dDriverAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/Job2dDriverAdapter.java
@@ -4,15 +4,18 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 /**
  * driver adapter to drawer with several bugs.
  */
 public class Job2dDriverAdapter extends DrawPanelController implements Job2dDriver {
 	private int startX = 0, startY = 0;
+	DrawPanelController drawPanelController;
 
 	public Job2dDriverAdapter() {
 		super();
+		drawPanelController = DrawerFeature.getDrawerController();
 	}
 
 	@Override
@@ -26,7 +29,7 @@ public class Job2dDriverAdapter extends DrawPanelController implements Job2dDriv
 		ILine line = LineFactory.getBasicLine();
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
-		drawLine(line);
+		drawPanelController.drawLine(line);
 		setPosition(x, y);
 
 	}
